127a0948f0f56f849aeb84557ea48919375f84d2
Tuấn Vương <vanhtuan0409@gmail.com>
2016-12-03 12:54:12 +0900

    add dynamic port

--- client/server.js
+++ client/server.js
@@ -25,11 +25,12 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, (err) => {
+const port = process.env.port || 3000;
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log(`Listening at http://localhost:${port}`);
 });
