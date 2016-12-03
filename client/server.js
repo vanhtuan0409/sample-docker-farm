2e7063d1a8338e929b0e90b903293781993aed50
Tuấn Vương <vanhtuan0409@gmail.com>
2016-12-03 13:00:42 +0900

    Change ENV VAR name

--- client/server.js
+++ client/server.js
@@ -25,7 +25,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
